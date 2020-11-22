3491a4731012e217bf00290fbb6dd9348db960c7
Rockhopper-Penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-11-22 15:38:14 +0900

    css削除

--- Vue/src/src/components/Main.vue
+++ Vue/src/src/components/Main.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <h1>メインページ</h1>
+    <h1>Voice Text音声合成</h1>
   </div>
 </template>
 
@@ -14,25 +14,10 @@ export default class Main extends Vue {
     super();
     const text = "ボイステキストクラス";
     const voiceText = new VoiceText(text);
-    console.log("テスト");
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
 </style>
