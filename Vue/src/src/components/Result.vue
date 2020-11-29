3b3b6eba698fb8b09ff15c15029d12d99e153ff9
rockhopper-penguin <46835999+rockhopper-penguin@users.noreply.github.com>
2020-11-29 20:50:01 +0900

    result.vue追加

---
+++ Vue/src/src/components/Result.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="result"></div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+
+@Component
+export default class Result extends Vue {
+  constructor() {
+    super();
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped></style>
